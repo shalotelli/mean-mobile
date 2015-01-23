ec7343d0bd15236734497f9b62be264e181cfd99
shalotelli <shaalibhai@unboxedtechnology.com>
2015-01-23 14:37:20 -0500

    refactor Crud & Restify services

--- api/services/Crud.js
+++ api/services/Crud.js
@@ -1,82 +1,81 @@
 var _ = require('lodash');
 
-var Crud = function (Model) {
+function Crud (Model) {
   this.Model = Model;
+}
 
-  return {
-    find: function (req, res) {
-      var criteria = {},
-          findMethod = 'find';
+Crud.prototype.find = function (req, res) {
+  var criteria = {},
+      findMethod = 'find';
 
-      if (req.params.id) {
-        criteria._id = req.params.id;
-        returnSingle = 'findOne';
+  if (req.params.id) {
+    criteria._id = req.params.id;
+    returnSingle = 'findOne';
+  }
+
+  if (! _.isEmpty(req.query)) {
+    criteria = _.extend(criteria, req.query);
+  }
+
+  return this.Model[findMethod](criteria, function (err, documents) {
+    if (err) {
+      return res.status(501).json(err);
+    }
+
+    return res.json(documents);
+  });
+};
+
+Crud.prototype.create = function (req, res) {
+  var document;
+
+  if (req.body) {
+    document = new this.Model(req.body);
+
+    document.save(function (err) {
+      if (err) {
+        return res.status(501).json(err);
       }
 
-      if (! _.isEmpty(req.query)) {
-        criteria = _.extend(criteria, req.query);
+      return res.json(document);
+    });
+  } else {
+    return res.status(400).json({ success: false });
+  }
+};
+
+Crud.prototype.update = function (req, res) {
+  if (req.params.id) {
+    this.Model.findOne({ _id: req.params.id }, function (err, document) {
+      if (err) {
+        return res.status(501).json(err);
       }
 
-      return this.Model[findMethod](criteria, function (err, documents) {
+      document = _.extend(document, req.body);
+
+      document.save(function (err) {
         if (err) {
           return res.status(501).json(err);
         }
-        return res.json(documents);
+
+        return res.json(document);
       });
-    },
+    });
+  }
+};
 
-    create: function (req, res) {
-      var document;
-
-      if (req.body) {
-        document = new this.Model(req.body);
-
-        document.save(function (err) {
-          if (err) {
-            return res.status(501).json(err);
-          }
-
-          return res.json(document);
-        });
-      } else {
-        return res.status(400).json({ success: false });
+Crud.prototype.delete = function (req, res) {
+  if (req.params.id) {
+    this.Model.findOneAndRemove({ _id: req.params.id }, function (err, document) {
+      if (err) {
+        return res.status(501).json(err);
       }
-    },
 
-    update: function (req, res) {
-      if (req.params.id) {
-        this.Model.findOne({ _id: req.params.id }, function (err, document) {
-          if (err) {
-            return res.status(501).json(err);
-          }
-
-          document = _.extend(document, req.body);
-
-          document.save(function (err) {
-            if (err) {
-              return res.status(501).json(err);
-            }
-
-            return res.json(document);
-          });
-        });
-      }
-    },
-
-    delete: function (req, res) {
-      if (req.params.id) {
-        this.Model.findOneAndRemove({ _id: req.params.id }, function (err, document) {
-          if (err) {
-            return res.status(501).json(err);
-          }
-
-          return res.json(document);
-        });
-      } else {
-        return res.status(400).json({ success: false });
-      }
-    }
-  };
+      return res.json(document);
+    });
+  } else {
+    return res.status(400).json({ success: false });
+  }
 };
 
 module.exports = Crud;
--- api/services/Restify.js
+++ api/services/Restify.js
@@ -2,9 +2,23 @@ module.exports = function (route, controller) {
   var path = '/' + route,
       pathWithId = path + '/:id';
 
-  app.get(path, controller.find);
-  app.get(pathWithId, controller.find);
-  app.post(path, controller.create);
-  app.put(pathWithId, controller.update);
-  app.delete(pathWithId, controller.delete);
+  app.get(path, function (req, res) {
+    controller.find(req, res);
+  });
+
+  app.get(pathWithId, function (req, res) {
+    controller.find(req, res);
+  });
+
+  app.post(path, function (req, res) {
+    controller.create(req, res);
+  });
+
+  app.put(pathWithId, function (req, res) {
+    controller.update(req, res);
+  });
+
+  app.delete(pathWithId, function (req, res) {
+    controller.delete(req, res);
+  });
 };
